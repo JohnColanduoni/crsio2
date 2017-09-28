250a1d69a51604a06fce9f3685fd9e4ca9763585
John Colanduoni <john@colanduoni.com>
2017-09-28 02:58:38 -0700

    Added error messages

--- api/c/src/windows/error.cc
+++ api/c/src/windows/error.cc
@@ -23,90 +23,86 @@ const char* ChromiumError::description() {
       return "bad parameters";
     case sandbox::SBOX_ERROR_UNSUPPORTED:
       return "unsupported";
-/*
-  SBOX_ERROR_NO_SPACE = 4,
-  // The ipc service requested does not exist.
-  SBOX_ERROR_INVALID_IPC = 5,
-  // The ipc service did not complete.
-  SBOX_ERROR_FAILED_IPC = 6,
-  // The requested handle was not found.
-  SBOX_ERROR_NO_HANDLE = 7,
-  // This function was not expected to be called at this time.
-  SBOX_ERROR_UNEXPECTED_CALL = 8,
-  // WaitForAllTargets is already called.
-  SBOX_ERROR_WAIT_ALREADY_CALLED = 9,
-  // A channel error prevented DoCall from executing.
-  SBOX_ERROR_CHANNEL_ERROR = 10,
-  // Failed to create the alternate desktop.
-  SBOX_ERROR_CANNOT_CREATE_DESKTOP = 11,
-  // Failed to create the alternate window station.
-  SBOX_ERROR_CANNOT_CREATE_WINSTATION = 12,
-  // Failed to switch back to the interactive window station.
-  SBOX_ERROR_FAILED_TO_SWITCH_BACK_WINSTATION = 13,
-  // The supplied AppContainer is not valid.
-  SBOX_ERROR_INVALID_APP_CONTAINER = 14,
-  // The supplied capability is not valid.
-  SBOX_ERROR_INVALID_CAPABILITY = 15,
-  // There is a failure initializing the AppContainer.
-  SBOX_ERROR_CANNOT_INIT_APPCONTAINER = 16,
-  // Initializing or updating ProcThreadAttributes failed.
-  SBOX_ERROR_PROC_THREAD_ATTRIBUTES = 17,
-  // Error in creating process.
-  SBOX_ERROR_CREATE_PROCESS = 18,
-  // Failure calling delegate PreSpawnTarget.
-  SBOX_ERROR_DELEGATE_PRE_SPAWN = 19,
-  // Could not assign process to job object.
-  SBOX_ERROR_ASSIGN_PROCESS_TO_JOB_OBJECT = 20,
-  // Could not assign process to job object.
-  SBOX_ERROR_SET_THREAD_TOKEN = 21,
-  // Could not get thread context of new process.
-  SBOX_ERROR_GET_THREAD_CONTEXT = 22,
-  // Could not duplicate target info of new process.
-  SBOX_ERROR_DUPLICATE_TARGET_INFO = 23,
-  // Could not set low box token.
-  SBOX_ERROR_SET_LOW_BOX_TOKEN = 24,
-  // Could not create file mapping for IPC dispatcher.
-  SBOX_ERROR_CREATE_FILE_MAPPING = 25,
-  // Could not duplicate shared section into target process for IPC dispatcher.
-  SBOX_ERROR_DUPLICATE_SHARED_SECTION = 26,
-  // Could not map view of shared memory in broker.
-  SBOX_ERROR_MAP_VIEW_OF_SHARED_SECTION = 27,
-  // Could not apply ASLR mitigations to target process.
-  SBOX_ERROR_APPLY_ASLR_MITIGATIONS = 28,
-  // Could not setup one of the required interception services.
-  SBOX_ERROR_SETUP_BASIC_INTERCEPTIONS = 29,
-  // Could not setup basic interceptions.
-  SBOX_ERROR_SETUP_INTERCEPTION_SERVICE = 30,
-  // Could not initialize interceptions. This usually means 3rd party software
-  // is stomping on our hooks, or can sometimes mean the syscall format has
-  // changed.
-  SBOX_ERROR_INITIALIZE_INTERCEPTIONS = 31,
-  // Could not setup the imports for ntdll in target process.
-  SBOX_ERROR_SETUP_NTDLL_IMPORTS = 32,
-  // Could not setup the handle closer in target process.
-  SBOX_ERROR_SETUP_HANDLE_CLOSER = 33,
-  // Cannot get the current Window Station.
-  SBOX_ERROR_CANNOT_GET_WINSTATION = 34,
-  // Cannot query the security attributes of the current Window Station.
-  SBOX_ERROR_CANNOT_QUERY_WINSTATION_SECURITY = 35,
-  // Cannot get the current Desktop.
-  SBOX_ERROR_CANNOT_GET_DESKTOP = 36,
-  // Cannot query the security attributes of the current Desktop.
-  SBOX_ERROR_CANNOT_QUERY_DESKTOP_SECURITY = 37,
-  // Cannot setup the interception manager config buffer.
-  SBOX_ERROR_CANNOT_SETUP_INTERCEPTION_CONFIG_BUFFER = 38,
-  // Cannot copy data to the child process.
-  SBOX_ERROR_CANNOT_COPY_DATA_TO_CHILD = 39,
-  // Cannot setup the interception thunk.
-  SBOX_ERROR_CANNOT_SETUP_INTERCEPTION_THUNK = 40,
-  // Cannot resolve the interception thunk.
-  SBOX_ERROR_CANNOT_RESOLVE_INTERCEPTION_THUNK = 41,
-  // Cannot write interception thunk to child process.
-  SBOX_ERROR_CANNOT_WRITE_INTERCEPTION_THUNK = 42,
-  // Cannot find the base address of the new process.
-  SBOX_ERROR_CANNOT_FIND_BASE_ADDRESS = 43,
-  // Placeholder for last item of the enum.
-  SBOX_ERROR_LAST*/
+    case sandbox::SBOX_ERROR_NO_SPACE:
+      return "no space";
+    case sandbox::SBOX_ERROR_INVALID_IPC:
+      return "invalid ipc";
+    case sandbox::SBOX_ERROR_FAILED_IPC:
+      return "failed ipc";
+    case sandbox::SBOX_ERROR_NO_HANDLE:
+      return "no handle";
+    case sandbox::SBOX_ERROR_UNEXPECTED_CALL:
+      return "unexpected call";
+    case sandbox::SBOX_ERROR_WAIT_ALREADY_CALLED:
+      return "wait already called";
+    case sandbox::SBOX_ERROR_CHANNEL_ERROR:
+      return "channel error";
+    case sandbox::SBOX_ERROR_CANNOT_CREATE_DESKTOP:
+      return "cannot create desktop";
+    case sandbox::SBOX_ERROR_CANNOT_CREATE_WINSTATION:
+      return "cannot create winstation";
+    case sandbox::SBOX_ERROR_FAILED_TO_SWITCH_BACK_WINSTATION:
+      return "failed to switch back winstation";
+    case sandbox::SBOX_ERROR_INVALID_APP_CONTAINER:
+      return "invalid AppContainer";
+    case sandbox::SBOX_ERROR_INVALID_CAPABILITY:
+      return "invalid capability";
+    case sandbox::SBOX_ERROR_CANNOT_INIT_APPCONTAINER:
+      return "cannot init AppContainer";
+    case sandbox::SBOX_ERROR_PROC_THREAD_ATTRIBUTES:
+      return "PROC_THREAD_ATTRIBUTES error";
+    case sandbox::SBOX_ERROR_CREATE_PROCESS:
+      return "process creation error";
+    case sandbox::SBOX_ERROR_DELEGATE_PRE_SPAWN:
+      return "delegate pre-spawn";
+    case sandbox::SBOX_ERROR_ASSIGN_PROCESS_TO_JOB_OBJECT:
+      return "could not assign process to job";
+    case sandbox::SBOX_ERROR_SET_THREAD_TOKEN:
+      return "could not set thread token";
+    case sandbox::SBOX_ERROR_GET_THREAD_CONTEXT:
+      return "could not get thread context";
+    case sandbox::SBOX_ERROR_DUPLICATE_TARGET_INFO:
+      return "duplicate target info";
+    case sandbox::SBOX_ERROR_SET_LOW_BOX_TOKEN:
+      return "failed to set low box token";
+    case sandbox::SBOX_ERROR_CREATE_FILE_MAPPING:
+      return "failed to create file mapping";
+    case sandbox::SBOX_ERROR_DUPLICATE_SHARED_SECTION:
+      return "duplicate shared section";
+    case sandbox::SBOX_ERROR_MAP_VIEW_OF_SHARED_SECTION:
+      return "failed to map view of shared section";
+    case sandbox::SBOX_ERROR_APPLY_ASLR_MITIGATIONS:
+      return "failed to apply ASLR";
+    case sandbox::SBOX_ERROR_SETUP_BASIC_INTERCEPTIONS:
+      return "failed to setup basic interceptions";
+    case sandbox::SBOX_ERROR_SETUP_INTERCEPTION_SERVICE:
+      return "failed to setup interception service";
+    case sandbox::SBOX_ERROR_INITIALIZE_INTERCEPTIONS:
+      return "failed to initialize interceptions";
+    case sandbox::SBOX_ERROR_SETUP_NTDLL_IMPORTS:
+      return "failed to setup ntdll imports";
+    case sandbox::SBOX_ERROR_SETUP_HANDLE_CLOSER:
+      return "failed to setup handle closer";
+    case sandbox::SBOX_ERROR_CANNOT_GET_WINSTATION:
+      return "cannot get winstation";
+    case sandbox::SBOX_ERROR_CANNOT_QUERY_WINSTATION_SECURITY:
+      return "cannot query winstation security";
+    case sandbox::SBOX_ERROR_CANNOT_GET_DESKTOP:
+      return "cannot get desktop";
+    case sandbox::SBOX_ERROR_CANNOT_QUERY_DESKTOP_SECURITY:
+      return "cannot query desktop security";
+    case sandbox::SBOX_ERROR_CANNOT_SETUP_INTERCEPTION_CONFIG_BUFFER:
+      return "cannot setup interception config buffer";
+    case sandbox::SBOX_ERROR_CANNOT_COPY_DATA_TO_CHILD:
+      return "cannot copy data to child";
+    case sandbox::SBOX_ERROR_CANNOT_SETUP_INTERCEPTION_THUNK:
+      return "cannot setup interception thunk";
+    case sandbox::SBOX_ERROR_CANNOT_RESOLVE_INTERCEPTION_THUNK:
+      return "cannot resolve interception thunk";
+    case sandbox::SBOX_ERROR_CANNOT_WRITE_INTERCEPTION_THUNK:
+      return "cannot write interception thunk";
+    case sandbox::SBOX_ERROR_CANNOT_FIND_BASE_ADDRESS:
+      return "cannot find base address";
     default:
       return "unknown";
   }
